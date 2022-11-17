6b4ce30e5f7fe54689a7990f65d71c4da4ff5ffc
zibo1370 <2393967571@qq.com>
2022-11-17 11:14:12 +0800

    Add files via upload

---
+++ js/jindu.js
@@ -0,0 +1,61 @@
+
+	$('#indicatorContainer1').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 20,
+        radius:80,
+        initValue: 40,
+        roundCorner : true,
+        percentage: true
+    });
+	var radial1 = $('#indicatorContainer1').data('radialIndicator');
+	radial1.animate(80); 
+$('#indicatorContainer2').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 20,
+        radius:80,
+        initValue: 40,
+        roundCorner : true,
+        percentage: true
+    });
+	var radial2 = $('#indicatorContainer2').data('radialIndicator');
+	radial2.animate(75); 	
+$('#indicatorContainer3').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 20,
+        radius:80,
+        initValue: 40,
+        roundCorner : true,
+        percentage: true
+    });
+	var radial3 = $('#indicatorContainer3').data('radialIndicator');
+	radial3.animate(73); 
+$('#indicatorContainer4').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 20,
+        radius:80,
+        initValue: 40,
+        roundCorner : true,
+        percentage: true
+    });
+	var radial4 = $('#indicatorContainer4').data('radialIndicator');
+	radial4.animate(80); 
+	$('#indicatorContainer5').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 20,
+        radius:80,
+        initValue: 40,
+        roundCorner : true,
+        percentage: true
+    });
+	var radial5 = $('#indicatorContainer5').data('radialIndicator');
+	radial5.animate(72); 
+	$('#indicatorContainer6').radialIndicator({
+        barColor: '#87CEEB',
+        barWidth: 20,
+        radius:80,
+        initValue: 40,
+        roundCorner : true,
+        percentage: true
+    });
+	var radial6 = $('#indicatorContainer6').data('radialIndicator');
+	radial6.animate(60); 
